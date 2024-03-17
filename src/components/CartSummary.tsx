46e5dd46bac6c8280d3e2369e359c331de1892b0
Mikhail Lagutik <lma3588329@gmail.com>
2024-03-17 21:12:12 +0300

    Implemented adaptive word endings

--- src/components/CartSummary.tsx
+++ src/components/CartSummary.tsx
@@ -2,15 +2,17 @@ import { Group, SimpleCell, Header } from '@vkontakte/vkui';
 import React, { useContext } from 'react';
 import { Context } from '..';
 import { observer } from 'mobx-react-lite';
+import { useEndingWord } from '../hooks/useEndingWord';
 
 const CartSummary = () => {
 
   const store = useContext(Context).store
+  const word = useEndingWord(store.totalNumberProducts)
 
   return (
     <Group separator='hide' style={{height: 'fit-content'}}>
       <SimpleCell after={<Header>{store.summaryCost} ₽</Header>}>
-        <Header subtitle="Без учета доставки" subtitleComponent="h3">Итого {store.totalNumberProducts} товара на сумму:</Header>
+        <Header subtitle="Без учета доставки" subtitleComponent="h3">Итого {store.totalNumberProducts} {word} на сумму:</Header>
       </SimpleCell>
     </Group>
   )
